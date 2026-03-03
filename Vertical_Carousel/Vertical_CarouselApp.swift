9e5468b760feb49471556892f0d4c6a42a4420a9
Ivan Voznyi <ivan.voznyi92@gmail.com>
2026-03-03 13:58:03 +0200

    Initial Commit

---
+++ Vertical_Carousel/Vertical_CarouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vertical_CarouselApp.swift
+//  Vertical_Carousel
+//
+//  Created by Ivan Voznyi on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct Vertical_CarouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
